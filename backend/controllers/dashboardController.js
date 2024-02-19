aab82ff6f398c446bd82cbff560b28bc5fb44c4e
Kombobill <billmadru25@gmail.com>
2024-02-20 00:04:01 +0300

    dashboard controller

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -1,11 +1,22 @@
-// Import DashboardData model if needed
-// const DashboardData = require('../models/DashboardData');
+// Import MySQL connection if needed
+// const connection = require('../database');
 
 const getDashboardData = (req, res) => {
-    // Logic to retrieve dashboard data
-  };
+  // Query to retrieve dashboard data from MySQL database
+  const query = 'SELECT * FROM dashboard_data';
 
-  module.exports = {
-    getDashboardData,
-  };
-  
+  // Execute the query
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error retrieving dashboard data:', err);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+
+    // If data is retrieved successfully, send it in the response
+    res.status(200).json({ dashboardData: results });
+  });
+};
+
+module.exports = {
+  getDashboardData,
+};
