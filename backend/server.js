31f14474d3e96c199bda35b53b2470955753285c
Kombobill <billmadru25@gmail.com>
2024-02-22 00:39:28 +0300

    complete functionalities

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,31 @@
 // server.js (or app.js)
+// const routes = require('./routes');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const routes = require('./routes');
-
-const app = express();
-
+const path = require('path');
 // Database connection setup for MySQL
 const mysql = require('mysql');
 
+
+
+
+const app = express();
+
+
+
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'your_mysql_username',
-  password: 'your_mysql_password',
-  database: 'your_database_name'
+  user: 'root',
+  password: 'Kombogates25',
+  database: 'mysql',
+  mysql_clear_password: () => () => Buffer.from('your_mysql_password'),
+
 });
+
+// Log the resolved path to the routes folder// Log he resolved path to the routes folder
+console.log('Resolved path to routes folder:', path.resolve(__dirname, './routes'));
+
 
 db.connect((err) => {
   if (err) {
@@ -27,10 +38,18 @@ db.connect((err) => {
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api', routes); // Mount the routes under /api
+// app.use('/api', routes);
+
+// Example error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+ // Mount the routes under /api
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
