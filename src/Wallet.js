7edc65ed59b621907d95875531a2016d6f646a34
Kombobill <billmadru25@gmail.com>
2024-02-29 16:42:44 +0300

    bit coin

--- src/Wallet.js
+++ src/Wallet.js
@@ -6,7 +6,7 @@ const WalletPage = () => {
     <div>
       <h1>Send Bitcoin Payment</h1>
       <p>Please send your payment to the following Bitcoin address:</p>
-      <p id="bitcoin-address">1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa</p>
+      <p id="bitcoin-address">1L5yYY6cxeHAEhZLa1Tj3ysdY3itCkdSLG</p>
       <button id="copy-button">Copy Address</button>
     </div>
   );
